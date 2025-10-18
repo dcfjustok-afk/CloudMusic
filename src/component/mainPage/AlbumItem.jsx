0624c1bf96b089a3dfc93a8d6120d812b2a80be3
charlie.dai <dcfjustok@gmail.com>
2025-10-19 00:23:45 +0800

    把个性化推荐组件 基本完成 ，对新碟上里的专辑进行初始化架

--- src/component/mainPage/AlbumItem.jsx
+++ src/component/mainPage/AlbumItem.jsx
@@ -1,6 +1,6 @@
 // AlbumItem.jsx
-export default function AlbumItem({ image, title ,playCount}) {
-  playCount =  "100万"
+export default function AlbumItem({ image, title, playCount }) {
+  playCount = "100万"
   return (
     <div className="album-item">
       <div className="album-img-wrapper">
@@ -12,4 +12,54 @@ export default function AlbumItem({ image, title ,playCount}) {
       </div>
     </div>
   );
+}
+export function PersonalAlbumItem({ image, title, playCount, reason }) {
+  return (
+    <div className="album-item">
+      <div className="album-img-wrapper">
+        <img src={image} alt={title} className="album-img" />
+        <div className="album-playcount">{playCount}</div>
+      </div>
+      <div className="album-info">
+        <div className="album-title">{title}</div>
+      </div>
+      <div className="album-info-reason">
+        <div className="album-reason">{reason}</div>
+      </div>
+    </div>
+  );
+}
+export function DateCard() {
+  const now = new Date();
+  const weekday = ["日", "一", "二", "三", "四", "五", "六"][now.getDay()];
+  const day = now.getDate();
+
+  return (
+    <div className="album-item">
+      <div className="date-card">
+        <div className="date-card-top">
+          星期{weekday}
+        </div>
+        <div className="date-card-bottom">
+          {day}
+        </div>
+      </div>
+      <div className="album-info">
+        <div className="album-title">每日歌曲推荐</div>
+      </div>
+      <div className="album-info-reason">
+        <div className="album-reason">根据你的口味生成,</div>
+        <div className="album-reason">每天6:00更新</div>
+
+      </div>
+    </div>
+
+  );
+}
+export function NewAlbunItem({ image, title, playCount, author }) {
+  return (
+    <div className="new-album-item">
+    </div>
+  )
+
 }
--- src/component/mainPage/PersonalizedRecommendation.jsx
+++ src/component/mainPage/PersonalizedRecommendation.jsx
@@ -1,10 +1,42 @@
 
 import Title from "../Title.jsx";
 import { TITLE_CONFIG } from '../Config/TitleConfig.js';
+import { PersonalAlbumItem } from "./AlbumItem.jsx";
+import { Flex } from "antd";
+import { DateCard } from "./AlbumItem.jsx";
 export default function PersonalizedRecommendation() {
+  const albumList = [
+    {
+      image: "img/album1.jpg",
+      title: "专辑名字1",
+      playCount: "100万",
+      reason: "猜你喜欢"
+    },
+    {
+      image: "img/album2.jpg",
+      title: "专辑名字2",
+      playCount: "101万",
+      reason: "你可能该感兴趣",
+    },
+    {
+      image: "img/album3.jpg",
+      title: "专辑名字3",
+      playCount: "102万",
+      reason: "根据常听推荐",
+    },
+  ]
+
   return (
     <div className="personalized-recommendation">
       <Title configId={TITLE_CONFIG.PERSONALIZED_RECOMMENDATION} />
+
+
+      <Flex className="personalized-album-list">
+        <DateCard />
+        {albumList.map((item, index) => (
+          <PersonalAlbumItem key={index} {...item} />
+        ))}
+      </Flex>
     </div>
   );
 }
